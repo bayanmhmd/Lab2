36158733f541203a654d8ece34f89666ee0264d0
bayan <bayana.mhmd99@gmail.com>
2020-01-25 08:07:04 +0200

    missing part 2

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -4,3 +4,71 @@ class Car {
 }
 
 module.exports = Car;
+
+
+
+'use strict';
+
+/////////////////////////////////// Vehicle Class ///////////////////////////
+class Vehicle {
+
+    constructor(name, wheels) {
+
+        this.name = name;
+        this.wheels = wheels;
+    }
+
+    drive() 
+    {
+        return 'Moving Forward';
+    }
+
+    stop() 
+    {
+        return 'Stopping';
+    }
+} 
+
+
+//////////////////////////////////////////// Car Class ///////////////////////////////////////////
+class Car extends Vehicle {
+
+    constructor(name, wheels) {
+
+        super(name, 4);        //Now  we use SUPER to be able to use methods in Vehicle Class
+        
+    }
+    run() 
+    {
+        super.drive();
+        super.stop();
+    }
+
+   
+}
+
+
+///////////////////////////////////////////// Motorcycle Class ///////////////////////////////////
+class Motorcycle extends Vehicle {
+
+    constructor(name, wheels) {
+       
+        super(name, 2);
+    }
+
+    
+    run() 
+    {
+        super.drive();
+        super.stop();
+    }
+
+    
+
+    wheelie() 
+    {
+        return 'Wheee!';
+    }
+} 
+
+module.exports = { Car, Motorcycle };
